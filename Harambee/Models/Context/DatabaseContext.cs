40ea0ee25aba516f464122e617a61135f5c2ab60
Sandile Ngubeni <Sandile.Ngubeni@multichoice.co.za>
2021-05-02 22:17:46 +0200

    Adds the customer endpoint.

---
+++ Harambee/Models/Context/DatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Harambee.Models.Context
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext() : base("DbConnection") { }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
