5680e0d6460ab1be65e0538a9031db3c371dfb4c
Sandile Ngubeni <Sandile.Ngubeni@multichoice.co.za>
2021-05-02 22:41:20 +0200

    Seeding customers

--- Harambee/Controllers/CustomersController.cs
+++ Harambee/Controllers/CustomersController.cs
@@ -17,12 +17,6 @@ namespace Harambee.Controllers
     {
         private DatabaseContext db = new DatabaseContext();
 
-        // GET: api/Customers
-        public IQueryable<Customer> GetCustomers()
-        {
-            return db.Customers;
-        }
-
         // GET: api/Customers/5
         [ResponseType(typeof(Customer))]
         public IHttpActionResult GetCustomer(int id)
@@ -34,86 +28,6 @@ namespace Harambee.Controllers
             }
 
             return Ok(customer);
-        }
-
-        // PUT: api/Customers/5
-        [ResponseType(typeof(void))]
-        public IHttpActionResult PutCustomer(int id, Customer customer)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            if (id != customer.Id)
-            {
-                return BadRequest();
-            }
-
-            db.Entry(customer).State = EntityState.Modified;
-
-            try
-            {
-                db.SaveChanges();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!CustomerExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return StatusCode(HttpStatusCode.NoContent);
-        }
-
-        // POST: api/Customers
-        [ResponseType(typeof(Customer))]
-        public IHttpActionResult PostCustomer(Customer customer)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            db.Customers.Add(customer);
-            db.SaveChanges();
-
-            return CreatedAtRoute("DefaultApi", new { id = customer.Id }, customer);
-        }
-
-        // DELETE: api/Customers/5
-        [ResponseType(typeof(Customer))]
-        public IHttpActionResult DeleteCustomer(int id)
-        {
-            Customer customer = db.Customers.Find(id);
-            if (customer == null)
-            {
-                return NotFound();
-            }
-
-            db.Customers.Remove(customer);
-            db.SaveChanges();
-
-            return Ok(customer);
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-
-        private bool CustomerExists(int id)
-        {
-            return db.Customers.Count(e => e.Id == id) > 0;
         }
     }
 }
---
+++ Harambee/Migrations/Configuration.cs
@@ -0,0 +1,29 @@
+﻿namespace Harambee.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Harambee.Models.Context.DatabaseContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "Harambee.Models.Context.DatabaseContext";
+        }
+
+        protected override void Seed(Harambee.Models.Context.DatabaseContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+
+            context.Customers.AddOrUpdate(c => c.Id,
+                new Models.Customer { Name = "Lucky", Surname = "July", Address = "2 Church Street, Sandton" },
+                new Models.Customer { Name = "Zama", Surname = "Zulu", Address = "23 Main Street, Melville" }
+                );
+        }
+    }
+}
